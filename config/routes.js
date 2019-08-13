ce5327c34e033ecd4b2f02c90bae8ff9fdd60959
chauhan_rakesh <rakeshchauhan7075@gmail.com>
2019-08-13 19:34:06 +0530

    api initialzation done and urls created

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,10 @@ var home = require('../app/controllers/home');
 var notes = require('../app/controllers/notes');
 var images = require('../app/controllers/images');
 var links = require('../app/controllers/links');
+var apihome = require('../app/api/home');
+var apinotes = require('../app/api/notes');
+var apilinks = require('../app/api/links');
+var apiimages = require('/app/api/images');
 //you can include all your controllers
 
 module.exports = function (app, passport) {
@@ -30,4 +34,27 @@ module.exports = function (app, passport) {
     app.put('/links/edit/:id',links.update_links);
     app.delete('/links/delete/:id',links.delete_links);
 
+    //api's
+
+    //home api
+
+    app.get('/api/home',apihome.home);
+
+    //notes api
+    app.get('/api/notes',apinotes.get_notes);
+    app.post('/api/notes',apinotes.add_notes);
+    app.get('/api/notes/edit/:id',apinotes.edit_notes);
+    app.put('/api/notes/edit/:id',apinotes.update_notes);
+    app.delete('/api/notes/:id',apinotes.delete_notes);
+
+    //images api
+    app.get('/api/images',apiimages.get_images);
+    app.post('/api/images',apiimages.add_images);
+
+    //links api
+    app.get('/api/links',apilinks.get_links);
+    app.post('/api/links',apilinks.add_links);
+    app.get('/api/links/:id',apilinks.edit_links);
+    app.put('/api/links/edit/:id',apilinks.update_links);
+    app.delete('/api/links/delete/:id',apilinks.delete_links);
 }
